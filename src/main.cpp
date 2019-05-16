b0d727d30e22570d81e597753933fe40c4895e52
Keewee <epsilonmajorquezero@gmail.com>
2019-05-16 15:37:12 +0200

    Refactor main to use new methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,39 @@
 #include "Vec3.h"
 #include "Color.h"
 #include "PpmDrawer.h"
+#include "Ray.h"
 
 using namespace std;
 
 int main()
 {
 	string imagesPath = "../images/";
-    int n = 1;
-	int nx = 200*n;
-    int ny = 100*n;
+	int n = 1;
+	int nx = 200 * n;
+	int ny = 100 * n;
+
+	Vec3 lowerLeftCorner(-2., -1., -1.);
+	Vec3 horizontal(4., 0., 0.);
+	Vec3 vertical(0., 2., 0.);
+	Vec3 origin(0., 0., 0.);
 
 	vector<Color> points = vector<Color>();
 
-    for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            Color col(float(i) / float(nx), float(j) / float(ny), 0.2);
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
 
-            points.push_back(col);
-        }
-    }
+			Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
 
-    string fileName = "02_color_gradient.ppm";
-    PpmDrawer drawer = PpmDrawer(imagesPath + fileName, nx, ny);
-    drawer.write(points);
+			Color color = r.getColor();
+			points.push_back(color);
+		}
+	}
+
+	string fileName = "02_blue_gradient.ppm";
+	PpmDrawer drawer = PpmDrawer(imagesPath + fileName, nx, ny);
+	drawer.write(points);
 }
