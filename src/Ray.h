3db69e408f48bc8ca054ed594c54670ddef5ae09
Keewee <epsilonmajorquezero@gmail.com>
2019-05-16 15:56:36 +0200

    Move Ray code to header for proper inline

--- src/Ray.h
+++ src/Ray.h
@@ -9,10 +9,46 @@ public:
 	Vec3 origin;
 	Vec3 direction;
 
-	Ray();
-	Ray(const Vec3& a, const Vec3& b);
+	Ray() = default;
 
-	Vec3 pointAtParameter(float t) const;
+	Ray(const Vec3 &a, const Vec3 &b)
+	{
+		this->origin = a;
+		this->direction = Vec3::unitVector(b);
+	}
 
-	Color getColor();
+	inline Vec3 pointAtParameter(float t) const
+	{
+		return this->origin + t * direction;
+	}
+
+	Color getColor()
+	{
+		if (hitSphere(Vec3(0., 0., -1.), 0.5))
+		{
+			return Color(1., 0., 0.);
+		}
+
+		return getBlueGradient();
+	}
+
+private:
+	inline bool hitSphere(const Vec3& center, float radius)
+	{
+		Vec3 oc = this->origin - center;
+		float a = dot(this->direction, this->direction);
+		float b = 2. * dot(oc, this->direction);
+		float c = dot(oc, oc) - radius * radius;
+		float discriminant = b * b - 4 * a * c;
+
+		return (discriminant > 0);
+	}
+
+	inline Color getBlueGradient()
+	{
+		float t = 0.5 * this->direction.y() + 1.0;
+		float inverseT = 1. - t;
+		return Color(inverseT + t * 0.5, inverseT + t * 0.7, inverseT + t);
+	}
 };
+
