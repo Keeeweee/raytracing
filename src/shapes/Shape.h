7b9af3bcd9af66782920b897dd7e77ff4af81b79
Keewee <epsilonmajorquezero@gmail.com>
2019-05-16 18:13:38 +0200

    Add Shape abstract class

---
+++ src/shapes/Shape.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include "../Vec3.h"
+
+class Ray;
+
+struct hitRecord
+{
+	float t;
+	Vec3 p;
+	Vec3 normal;
+};
+
+class Shape
+{
+public:
+	virtual bool hit(const Ray& ray, float tMin, float tMax, hitRecord& hit) const = 0;
+
+};
